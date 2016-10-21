c7650de301c63571727b29d0865993252380f038
Mirim An <mimicat@Mirims-MacBook-Air.local>
2016-10-21 17:29:11 -0400

    Understood

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -2,18 +2,24 @@
 
 import UIKit
 
+protocol TextDescription {
+    var textDescription : String { get }
+}
 
 
-class Person {
+class Person:TextDescription {
     
+    //properties
     var name: String
     var height: Double
     var favoriteFood: String
     
-    var description: String {
-        return "Name: \(jim.name) \nHeight: \(jim.height) \nFavorite Food: \(jim.favoriteFood)"
+    //conforming to the protocol TextDescription
+    var textDescription: String {
+        return "Name: \(self.name) \nHeight: \(self.height) \nFavorite Food: \(self.favoriteFood)"
     }
     
+    //init
     init(name: String, height: Double, favoriteFood: String) {
         self.name = name
         self.height = height
@@ -24,11 +30,7 @@ class Person {
 
 let jim = Person(name: "Jim", height: 64.0, favoriteFood: "Pasta")
 
-print(jim.description)
-//Name: Jim
-//Height: 64.0
-//Favorite Food: Pasta
-
+print(jim.textDescription)
 
 
 protocol PrettyDescription {
@@ -38,12 +40,12 @@ protocol PrettyDescription {
 }
 
 
-
-class Animal {
+class Animal:PrettyDescription {
     
     var name: String
     var height: Double
     var favoriteFood: String
+    var description: String = " Animals are happiness"
     
     init(name: String, height: Double, favoriteFood: String) {
         self.name = name
@@ -52,13 +54,4 @@ class Animal {
     }
     
 }
-
-let prettyThings: [PrettyDescription] = []
-
-for prettyThing in prettyThings {
-    
-    print(prettyThing.description)
-    
-}
-
 
